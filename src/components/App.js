919a627c1ceee4cf2b38e83ebbca8004c9bdb804
Ronan Parkinson <parkinson95@hotmail.co.uk>
2021-02-21 21:37:52 +0000

    Add files via upload
    adding statistics functions to scoreboard

--- src/components/App.js
+++ src/components/App.js
@@ -49,7 +49,7 @@ class App extends Component{
         return( 
             <div className="scoreboard">
                 <Header title="Scoreboard" 
-                totalPlayers={this.state.players.length} />
+                players={this.state.players} />
     
                 {/* Players list */}
                 {this.state.players.map( (player, index)  =>
